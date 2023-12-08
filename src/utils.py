c9fc76bd56c25c7d4e49da593fde208087e1d1c3
CopainMalin <bgautier3800@gmail.com>
2023-12-08 14:30:59 +0100

    Adding tool to transform dim reduction to dataset nice to plot

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,21 @@
 from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
+from numpy.typing import ArrayLike
+from typing import Iterable
 
 
 def transform_nixtla_format(nixtla_df: DataFrame, column: str = "H1") -> DataFrame:
     sub_df = nixtla_df[nixtla_df["unique_id"] == column]
     return DataFrame({"ds": sub_df["ds"], column: sub_df["y"]})
+
+
+def build_reduc_dim_df(reduc: ArrayLike, serie_names: Iterable) -> DataFrame:
+    return DataFrame(
+        {
+            "fst_dim": reduc[:, 0],
+            "snd_dim": reduc[:, 1],
+            "trd_dim": reduc[:, 2],
+            "Name": serie_names,
+            "Style": LabelEncoder().fit_transform(serie_names),
+        }
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,18 @@
 from precomputed_ressources.loader import load_transformed_h1, load_hourly_m4_dataset
-from src.utils import transform_nixtla_format
+from src.utils import transform_nixtla_format, build_reduc_dim_df
+from numpy.random import rand
 
 
 def test_transform_nixtla_format():
     targeted_col = load_transformed_h1()
     original_df = load_hourly_m4_dataset()
     assert targeted_col.equals(transform_nixtla_format(original_df, "H1"))
+
+
+def test_build_reduc_df():
+    original_data = rand(10, 3)
+    names = [*["H1"] * 3, *["H2"] * 3, *["H3"] * 4]
+    reducted_df = build_reduc_dim_df(original_data, names)
+    assert (reducted_df.iloc[:, :3].values == original_data).all()
+    assert (names == reducted_df["Name"]).all()
+    assert (reducted_df["Style"] == [*[0] * 3, *[1] * 3, *[2] * 4]).all()
